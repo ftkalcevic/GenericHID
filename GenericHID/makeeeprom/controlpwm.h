9c7b68de8d8910bb93bedf16e7ed299842ed0ea6
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2009-09-27 03:39:39 +0000

    Add copyright

--- GenericHID/makeeeprom/controlpwm.h
+++ GenericHID/makeeeprom/controlpwm.h
@@ -1,4 +1,22 @@
-#pragma once
+// generichid, DIY HID device 
+// Copyright (C) 2009, Frank Tkalcevic, www.franksworkshop.com
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _CONTROLPWM_H_
+#define _CONTROLPWM_H_
+
 #include "control.h"
 
 class ControlPWM : public Control
@@ -20,3 +38,5 @@ private:
     byte m_nBits;
     uint16_t m_nResolution;
 };
+
+#endif
