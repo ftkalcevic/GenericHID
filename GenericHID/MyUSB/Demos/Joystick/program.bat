769ce4aee0d13105733baa350bbf70d670beb188
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2010-04-18 04:48:27 +0000

    Remove My USB

--- GenericHID/MyUSB/Demos/Joystick/program.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Atmel\AVR Tools\JTAGICEmkII\jtagiceii.exe" -c usb -d at90usb1287 -mj -R -e -pb -vb -if "D:\Projects\Gecko\MyUSB\Demos\Joystick\Joystick.hex" -ie "D:\Projects\Gecko\MyUSB\Demos\Joystick\eeprom.hex" 
-"C:\Program Files\Atmel\AVR Tools\JTAGICEmkII\jtagiceii.exe" -c usb -d at90usb1287 -mj -R -pf -vf -if "D:\Projects\Gecko\MyUSB\Demos\Joystick\bl_usb_128k.a90"
