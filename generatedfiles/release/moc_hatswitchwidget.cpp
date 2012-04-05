23c3e275f7910f036d9d8f59f80e0f158710c35f
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2012-04-05 23:22:56 +0000

    Fix bugs with Function Set Report. Remove Function Set from initialisation.

--- generatedfiles/release/moc_hatswitchwidget.cpp
+++ generatedfiles/release/moc_hatswitchwidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'hatswitchwidget.h'
 **
-** Created: Thu 5. Apr 08:25:34 2012
+** Created: Fri 6. Apr 09:16:46 2012
 **      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
 **
 ** WARNING! All changes made in this file will be lost!
