4b554d35c62301536848ad0704678979c862a6aa
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2012-04-10 14:33:39 +0000

    Update to build against LinuxCNC 2.5

--- generatedfiles/debug/moc_lcdwidget.cpp
+++ generatedfiles/debug/moc_lcdwidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'lcdwidget.h'
 **
-** Created: Tue Apr 10 23:48:57 2012
+** Created: Wed Apr 11 00:07:24 2012
 **      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
 **
 ** WARNING! All changes made in this file will be lost!
--- generatedfiles/release/moc_hatswitchwidget.cpp
+++ generatedfiles/release/moc_hatswitchwidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'hatswitchwidget.h'
 **
-** Created: Tue Apr 10 23:48:51 2012
+** Created: Wed Apr 11 00:07:17 2012
 **      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
 **
 ** WARNING! All changes made in this file will be lost!
