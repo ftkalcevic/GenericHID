bcd95c3076839dc99c13ab5cc9f1cb5466f4218f
frankt <frankt@e4c7163f-7ddd-7b40-8da7-0e3d6902e64e>
2013-11-30 21:38:07 +0000

    dont detach interface if we don't need to.  Add more debug

--- GenericHID/dfuprogrammer/teensyprogrammer.cpp
+++ GenericHID/dfuprogrammer/teensyprogrammer.cpp
@@ -45,7 +45,7 @@ TeensyProgrammer::TeensyProgrammer(void)
     m_bootloader_bottom = m_flash_address_top - bootloader_size;
     m_bootloader_top = m_flash_address_top;
     m_flash_address_top -= (bootloader_size + 1);
-
+    
 }
 
 TeensyProgrammer::~TeensyProgrammer(void)
@@ -55,7 +55,7 @@ TeensyProgrammer::~TeensyProgrammer(void)
 bool TeensyProgrammer::GetDevice()
 {
     if ( teensy_open() )
-	return true;
+        return true;
     return false;
 }
 
@@ -73,7 +73,7 @@ bool TeensyProgrammer::EraseDevice()
 bool TeensyProgrammer::StartProgramming(IntelHexBuffer &memory)
 {
     (*m_Callback)(m_user_data, 0 );
-
+    
     // program the data
     unsigned char buf[260];
     bool first_block=true;
@@ -81,35 +81,35 @@ bool TeensyProgrammer::StartProgramming(IntelHexBuffer &memory)
     int code_size = m_flash_address_top;
     for ( unsigned int addr = 0; addr < m_flash_address_top; addr += block_size ) 
     {
-	(*m_Callback)(m_user_data, 100 * addr / m_flash_address_top );
-	if ( addr > 0 && !memory.isInUse(addr, addr + block_size - 1) ) 
-	{
-	    // don't waste time on blocks that are unused,
-	    // but always do the first one to erase the chip
-	    continue;
-	}
-	
-	if (code_size < 0x10000) 
-	{
-	    buf[0] = (unsigned char)(addr & 255);
-	    buf[1] = (unsigned char)((addr >> 8) & 255);
-	} 
-	else 
-	{
-	    buf[0] = (unsigned char)((addr >> 8) & 255);
-	    buf[1] = (unsigned char)((addr >> 16) & 255);
-	}
-	for ( int i = 0; i < block_size; i++ )
-	    buf[i+2] = (unsigned char)memory.data().data()[addr+i];
-	
-	int r = teensy_write(buf, block_size + 2, first_block ? 3.0 : 0.25);
-
-	if (!r) 
-	{
-	    ERROR_MSG( "Error writing to Teensy.\n" );
-	    return false;
-	}
-	first_block = false;
+        (*m_Callback)(m_user_data, 100 * addr / m_flash_address_top );
+        if ( addr > 0 && !memory.isInUse(addr, addr + block_size - 1) ) 
+        {
+            // don't waste time on blocks that are unused,
+            // but always do the first one to erase the chip
+            continue;
+        }
+        
+        if (code_size < 0x10000) 
+        {
+            buf[0] = (unsigned char)(addr & 255);
+            buf[1] = (unsigned char)((addr >> 8) & 255);
+        } 
+        else 
+        {
+            buf[0] = (unsigned char)((addr >> 8) & 255);
+            buf[1] = (unsigned char)((addr >> 16) & 255);
+        }
+        for ( int i = 0; i < block_size; i++ )
+            buf[i+2] = (unsigned char)memory.data().data()[addr+i];
+        
+        int r = teensy_write(buf, block_size + 2, first_block ? 3.0 : 0.25);
+        
+        if (!r) 
+        {
+            ERROR_MSG( "Error writing to Teensy.\n" );
+            return false;
+        }
+        first_block = false;
     }
     (*m_Callback)(m_user_data, 100  );
     return true;
@@ -124,17 +124,17 @@ bool TeensyProgrammer::EnterApplicationMode(ResetMode::ResetMode /*mode*/, unsig
 {
     unsigned char buf[260];
     int block_size = m_flash_page_size*2;
-
+    
     //   if ( mode == ResetMode::Hard )
     //   {
     //hard_reboot();
     //   }
     //   else
     {
-	buf[0] = 0xFF;
-	buf[1] = 0xFF;
-	memset(buf + 2, 0, sizeof(buf) - 2);
-	teensy_write(buf, block_size + 2, 0.25);
+        buf[0] = 0xFF;
+        buf[1] = 0xFF;
+        memset(buf + 2, 0, sizeof(buf) - 2);
+        teensy_write(buf, block_size + 2, 0.25);
     }
     return true;
 }
@@ -143,16 +143,16 @@ bool TeensyProgrammer::EnterApplicationMode(ResetMode::ResetMode /*mode*/, unsig
 libusb_device_handle * TeensyProgrammer::open_usb_device(int vid, int pid)
 {
     libusb_device_handle *h;
-
+    
     libusb_init(NULL);
     libusb_device **devices = NULL;
     int device_count = libusb_get_device_list( NULL, &devices);
-
+    
     DEBUG_MSG( "nSearching for Teensy USB device:" );
     for ( int i = 0; i < device_count; i++ )
     {
         libusb_device *dev = devices[i];
-
+        
         libusb_device_descriptor desc;
         int r = libusb_get_device_descriptor(dev, &desc);
         if (r == 0)
@@ -161,31 +161,32 @@ libusb_device_handle * TeensyProgrammer::open_usb_device(int vid, int pid)
             if (desc.idVendor != vid) continue;
             if (desc.idProduct != pid) continue;
             h = NULL;
-            libusb_open(dev, &h);
-            if (!h) 
+            r = libusb_open(dev, &h);
+            if (r != 0 ) 
             {
-                ERROR_MSG( "Found device but unable to open" );
+                ERROR_MSG( QString("Found device but unable to open: %1").arg(r) );
                 continue;
             }
-
+            
             libusb_set_configuration(h, 1);
-            r = libusb_detach_kernel_driver(h, 0);
-            if (r < 0)
+            r = libusb_claim_interface(h, 0);
+            if ( r != 0 )
             {
-                libusb_close(h);
-                ERROR_MSG( "Device is in use" );
-                continue;
+                r = libusb_detach_kernel_driver(h, 0);
+                if (r < 0)
+                {
+                    libusb_close(h);
+                    ERROR_MSG( QString("Device is in use: %1").arg(r) );
+                    continue;
+                }
+                r = libusb_claim_interface(h, 0);
             }
         }
-
-        // Mac OS-X - removing this call to usb_claim_interface() might allow
-        // this to work, even though it is a clear misuse of the libusb API.
-        // normally Apple's IOKit should be used on Mac OS-X
-        r = libusb_claim_interface(h, 0);
-        if (r < 0)
+        
+        if (r != 0)
         {
             libusb_close(h);
-            ERROR_MSG( "Unable to claim interface, check USB permissions" );
+            ERROR_MSG( QString("Unable to claim interface, check USB permissions: %1").arg(r) );
             continue;
         }
         return h;
@@ -206,7 +207,7 @@ int TeensyProgrammer::teensy_open(void)
 int TeensyProgrammer::teensy_write(void *buf, int len, double timeout)
 {
     int r;
-
+    
     if (!libusb_teensy_handle) return 0;
     r = libusb_control_transfer(libusb_teensy_handle, 0x21, 9, 0x0200, 0, (unsigned char *)buf,
                                 len, (int)(timeout * 1000.0));
@@ -224,15 +225,15 @@ void TeensyProgrammer::teensy_close(void)
 
 int TeensyProgrammer::hard_reboot(void)
 {
-    libusb_device_handle *rebootor;
-    int r;
+//    libusb_device_handle *rebootor;
+//    int r;
 
-    rebootor = open_usb_device(0x16C0, 0x0477);
-    if (!rebootor) return 0;
-    r = libusb_control_transfer(rebootor, 0x21, 9, 0x0200, 0, (unsigned char *)"reboot", 6, 100);
-    libusb_release_interface(rebootor, 0);
-    libusb_close(rebootor);
-    if (r < 0) return 0;
+//    rebootor = open_usb_device(0x16C0, 0x0477);
+//    if (!rebootor) return 0;
+//    r = libusb_control_transfer(rebootor, 0x21, 9, 0x0200, 0, (unsigned char *)"reboot", 6, 100);
+//    libusb_release_interface(rebootor, 0);
+//    libusb_close(rebootor);
+//    if (r < 0) return 0;
     return 1;
 }
 
